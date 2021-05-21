1829effb697b856eb97265999516baef835da7fc
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2021-05-20 23:19:29 -0300

    test: ensure CustomerController returns 400 if no name is provided

---
+++ src/presentation/controllers/customer.spec.ts
@@ -0,0 +1,38 @@
+import { CustomerController } from './customer'
+import { MissingParamError } from '../errors'
+
+interface sutTypes {
+  sut: CustomerController
+}
+
+const makeSut = (): sutTypes => {
+  const sut = new CustomerController()
+
+  return {
+    sut
+  }
+}
+
+describe('SignUp Controller', () => {
+  test('Shoud return 400 if no name is provided', () => {
+    const { sut } = makeSut()
+
+    const httpResquest = {
+      body: {
+        corporateName: 'any corporate name',
+        email: 'any_email@mail.com',
+        cnpj: 99999999999999,
+        cpf: 99999999999,
+        phone: 999999999999,
+        password: 'any password',
+        passwordConfirmation: 'any password confirmation',
+        sendEmailConfirmation: true
+      }
+    }
+
+    const httpResponse = sut.handle(httpResquest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('name'))
+  })
+})
