e7a83acfdbc45d42c08c92356b53e23a6cdd866f
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2021-05-30 11:50:12 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParses } from '../middlewares/body-parser'
+import { cors } from '../middlewares/cors'
 
 export default (app: Express): void => {
   app.use(bodyParses)
+  app.use(cors)
 }
