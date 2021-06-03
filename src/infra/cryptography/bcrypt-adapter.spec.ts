53afd619dd3b589a1dcb505203814bba3ee97606
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2021-06-03 17:07:14 -0300

    test: ensure BcryptAdapter throws if compare throws

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -32,7 +32,7 @@ describe('Bcrypt Adapter', () => {
     expect(hash).toBe('hash')
   })
 
-  // test('should throw if bcrypt throws ', async () => {
+  // test('should throw if hash throws ', async () => {
   //   const sut = makeSut()
   //   jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
   //   const promisse = sut.hash('any_value')
@@ -57,5 +57,12 @@ describe('Bcrypt Adapter', () => {
   //   jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise(resolve => resolve(false)))
   //   const isValid = await sut.compare('any_value', 'any_hash')
   //   expect(isValid).toBe(false)
+  // })
+
+  // test('should throw if comoare throws ', async () => {
+  //   const sut = makeSut()
+  //   jest.spyOn(bcrypt, 'compare').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+  //   const promisse = sut.compare('any_value', 'any_hash')
+  //   await expect(promisse).rejects.toThrow()
   // })
 })
