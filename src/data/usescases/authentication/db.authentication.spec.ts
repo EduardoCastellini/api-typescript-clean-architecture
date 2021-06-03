5b7f8698bca62c4b1ede0d9daf9ac0adfdb4da77
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2021-06-03 14:51:33 -0300

    test: ensure DbAuthentication returns a token on success

--- src/data/usescases/authentication/db.authentication.spec.ts
+++ src/data/usescases/authentication/db.authentication.spec.ts
@@ -123,4 +123,10 @@ describe('dbAuthentication UseCase', () => {
     const promisse = sut.auth(makeFakeAuthentication())
     await expect(promisse).rejects.toThrow()
   })
+
+  test('Should return a token on success', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe('any_token')
+  })
 })
