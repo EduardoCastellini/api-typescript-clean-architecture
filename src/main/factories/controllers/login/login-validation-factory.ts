c95eeed81ae21c5b2d33bad30a84c3234ef1677e
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2021-06-05 20:29:43 -0300

    refactor: move validation to its own layer

--- src/main/factories/controllers/login/login-validation-factory.ts
+++ src/main/factories/controllers/login/login-validation-factory.ts
@@ -1,6 +1,6 @@
-import { ValidationComposite, EmailValidation, RequeiredFieldValidation } from '../../../../presentation/helpers/validators'
+import { ValidationComposite, EmailValidation, RequeiredFieldValidation } from '../../../../validation/validators'
 import { Validation } from '../../../../presentation/protocols/validation'
-import { EmailValidatorAdapter } from '../../../adapters/validators/email-validator-adapter'
+import { EmailValidatorAdapter } from '../../../../infra/validators/email-validator-adapter'
 
 export const makeLoginValidation = (): ValidationComposite => {
   const validations: Validation[] = []
--- src/validation/validators/required-field-validation.spec.ts
+++ src/validation/validators/required-field-validation.spec.ts
@@ -1,4 +1,4 @@
-import { MissingParamError } from '../../errors'
+import { MissingParamError } from '../../presentation/errors'
 import { RequeiredFieldValidation } from './required-field-validation'
 
 const makeSut = (): RequeiredFieldValidation => {
