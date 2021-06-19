28820e9ed209dd609d579cd2a1e0b9bd875a053b
Eduardo Castellini <eduardo.castellini@tecnospeed.com.br>
2021-06-19 14:05:37 -0300

    test: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -78,4 +78,28 @@ describe('Account Mongo Repository', () => {
       expect(account.accessToken).toBe('any_token')
     })
   })
+
+  describe('loadByToken()', () => {
+    test('should return an account on loadBytoken without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@gmail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@gmail.com')
+      expect(account.password).toBe('any_password')
+    })
+
+    // test('should return null if loadByEmail fails', async () => {
+    //   const sut = makeSut()
+    //   const account = await sut.loadByEmail('any_email@gmail.com')
+    //   expect(account).toBeFalsy()
+    // })
+  })
 })
